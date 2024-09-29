bbf8b99e0c06429fc3dd1b2b0022f11604207f9d
Amani <amanialshmali7@gmail.com>
2024-09-29 15:08:20 +0400

    adding calss person

---
+++ HospitalManagementSystem/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem
+{
+    public class Person
+    {
+        public string Name { get; set; }    
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Person(string Name , int Age , string Gender)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Gender = Gender;
+
+        }
+
+        public void DisplayInfo() 
+        
+        {
+            Console.WriteLine($"Name : {Name} | Age : {Age} | Gender {Gender}");
+                   
+        }
+    }
+}
