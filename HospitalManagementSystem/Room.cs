36236bba168c9028c94be50130fa291b887d3670
Amani <amanialshmali7@gmail.com>
2024-09-30 09:50:19 +0400

    add class Room

---
+++ HospitalManagementSystem/Room.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem
+{
+    public class Room
+    {
+
+        public int RoomNumber { get; set; }
+        public bool IsOccupied { get; set; }
+        public enum RoomType
+        {
+            General,
+            ICU,
+            OperationTheater
+        }
+        public RoomType Type { get; set; }
+
+        public void OccupyRoom()
+        {
+            if (IsOccupied)
+            {
+
+                Console.WriteLine($"Room {RoomNumber} is now occupied.");
+
+
+            }
+
+            else 
+            {
+                Console.WriteLine($"Room {RoomNumber} is already occupied.");
+
+            }
+        
+        
+        }
+
+        public void VacateRoom() 
+        {
+
+            if (IsOccupied)
+            {
+                Console.WriteLine($"Room {RoomNumber} is now available.");
+
+
+            }
+            else 
+            {
+
+                Console.WriteLine($"Room {RoomNumber} is already available.");
+
+            }
+        
+        }
+
+    }
+}
