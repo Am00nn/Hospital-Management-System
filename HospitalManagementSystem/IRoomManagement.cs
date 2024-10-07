cd3747d75f7e8c53d2c15e79f193e79c750853e1
Amani <amanialshmali7@gmail.com>
2024-10-07 16:29:42 +0400

    add IRoomManagement interface

---
+++ HospitalManagementSystem/IRoomManagement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem
+{
+    public interface IRoomManagement
+    {
+        void OccupyRoom();
+        void VacateRoom();
+        bool IsRoomOccupied();
+        void DisplayRoomInfo();
+    }
+}
